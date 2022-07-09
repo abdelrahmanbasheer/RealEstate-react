b7e2e9db059242084a93886b60f3fa4db6920b7f
aslw1 <Abdelrahman.Eldahab@student.aast.edu>
2022-07-09 07:31:31 +0200

    m

---
+++ components/Navbar.jsx
@@ -0,0 +1,39 @@
+import Link  from "next/link"
+import {Menu,MenuButton,MenuList,MenuItem,IconButton,Flex,Box,Spacer} from "@chakra-ui/react"
+import {FcMenu,FcHome,FcAbout} from "react-icons/fc"
+import {BsSearch} from "react-icons/bs"
+import {Fikey} from "react-icons/fi"
+
+const Navbar= ()=>(
+
+<Flex p="2" borderBottom="1px" borderColor="gray.100">
+<Box fontSize="3xl" color="blue.400" fontWeight="bold">
+<Link href="/" paddingLeft="2">EMAAR PROPERTIES
+</Link>
+</Box>
+<Spacer/>
+<Box>
+    <Menu>
+        <MenuButton as={IconButton} icon={<FcMenu/>} var="outline" color="red.400">
+            <MenuList>
+                <Link href="/" passHref>
+                    <MenuItem icon={<FcHome/>}>Home</MenuItem>
+                </Link>
+                <Link href="/search" passHref>
+                    <MenuItem icon={<BsSearch/>}>Search</MenuItem>
+                </Link>
+                <Link href="/search?purpose=for-sale" passHref>
+                    <MenuItem icon={<FcAbout/>}>Buy properties</MenuItem>
+                </Link>
+                <Link href="/search?purpose=for-rent" passHref>
+                    <MenuItem icon={<Fikey/>}>Rent properties</MenuItem>
+                </Link>
+            </MenuList>
+        </MenuButton>
+    </Menu> 
+</Box>
+</Flex>
+
+
+);
+export default Navbar
