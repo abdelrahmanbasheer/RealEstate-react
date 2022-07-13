e23abcecd17906bac295e0c58430d6b01863023a
aslw1 <Abdelrahman.Eldahab@student.aast.edu>
2022-07-13 09:05:11 +0200

    final edit

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,17 +1,23 @@
 import Link  from "next/link"
-import {Menu,MenuButton,MenuList,MenuItem,IconButton,Flex,Box,Spacer} from "@chakra-ui/react"
+import {Menu,MenuButton,MenuList,MenuItem,IconButton,Flex,Box,Spacer, transition} from "@chakra-ui/react"
 import {FcMenu,FcHome,FcAbout} from "react-icons/fc"
 import {BsSearch} from "react-icons/bs"
 import {FiKey} from "react-icons/fi"
+import { useMediaQuery } from "@chakra-ui/react"
 
-const Navbar= ()=>(
+const Navbar= ()=>{
+    const [isSmallScreen] = useMediaQuery("(max-width: 768px)")
+    return(
+   
 <Flex p="2" borderBottom="1px" borderColor="gray.100">
-<Box fontSize="3xl" color="blue.400" fontWeight="bold">
-<Link href="/" paddingLeft="2">EMAAR PROPERTIES
+<Box fontSize={{lg:"3xl",md:"xl",sm:"medium"}} color="blue.400" fontWeight="bold">
+<Link href="/" paddingLeft="2" >EMAAR PROPERTIES
 </Link>
 </Box>
 <Spacer/>
-<Box>
+{
+    isSmallScreen ?
+     <Box>
     <Menu>
         <MenuButton as={IconButton} icon={<FcMenu/>} var="outline" color="red.400">  </MenuButton>
             <MenuList>
@@ -31,7 +37,41 @@ const Navbar= ()=>(
       
     </Menu> 
 </Box>
+:
+<Flex gap="3" >
+<Link href="/" passHref>
+    
+<Box fontSize="2xl" cursor="pointer" _hover={{
+    color:'#1e86f1',
+    transition: "color 0.2s"
+}}
+ fontWeight="bold" m="3">Home</Box>
+</Link>
+<Link href="/search" passHref>
+<Box fontSize="2xl" cursor="pointer" _hover={{
+    color:'#1e86f1',
+    transition: "color 0.2s"
+}} fontWeight="bold"m="3">Search</Box>
+</Link>
+<Link href="/search?purpose=for-sale" passHref>
+<Box fontSize="2xl" cursor="pointer" _hover={{
+    color:'#1e86f1',
+    transition: "color 0.2s"
+}} fontWeight="bold" m="3">Buy Props.</Box>
+</Link>
+<Link href="/search?purpose=for-rent" passHref>
+<Box fontSize="2xl" cursor="pointer" _hover={{
+    color:'#1e86f1',
+    transition: "color 0.2s"
+}} fontWeight="bold" m="3">Rent Props. </Box>
+</Link>
 </Flex>
-);
+
+}
+
+
+</Flex>
+    )
+};
 
 export default Navbar
